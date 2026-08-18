f4b702ae93dd531af7fbf018e6afb3f36b076623
Frank Lind <flind@haystack.mit.edu>
2026-08-18 19:31:58 -0400

    Fixup proxy for marimo

--- src/astra/src/astra-ui/theme.py
+++ src/astra/src/astra-ui/theme.py
@@ -174,11 +174,12 @@ def _nav_links() -> None:
     _separator()
 
     _section_label("TOOLS")
-    
+
+    # note this link assumes a proxy server mapping 2719 on the server to 2718 occupied by marimo
     _external_link_button(
         icon_name  = "science",
         label      = "Notebook",
-        url        = "window.open('http://' + window.location.hostname + ':2718/', '_blank');",
+        url        = "window.open('http://' + window.location.hostname + ':2719/', '_blank');",
         sublabel   = "Python notebook",
         icon_color = "text-violet-400",
     )
